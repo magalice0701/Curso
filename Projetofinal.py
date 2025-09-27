25ec1232e92279fcad90f7698678876f00fbec09
Alice ⋆˚꩜｡ <magalice07@gmail.com>
2025-09-26 21:08:37 -0300

    Projetofinal.py

--- Projetofinal.py
+++ Projetofinal.py
@@ -1,13 +1,66 @@
 import tkinter as tk 
-from tkinter import ttk
 from tkinter import messagebox
 
-janela = tk.Tk
-janela.title('Banco')
-janela.geometry('300x200')
+def abrir_pix():
+    top = tk.Toplevel()
+    top.title("Pix")
+    top.geometry("400x300")
+    top.configure(bg='#9932CC')
 
-tk.Label(janela, text= 'Olá, bem-vindo ao banco!').pack(pady=20)
-tk.Button(janela, text='Pix')
+janela = tk.Tk()
+janela.title("Banco")
+janela.geometry("400x300")
+janela.configure(bg="#9932CC")
+
+texto = tk.Label(
+    janela, 
+    text='Bem-vindo(a) ao banco!!',
+    fg='white', 
+    bg="#9932CC",
+    font=('Verdana', 16, 'bold') 
+)
+texto.grid(row=0, column=0, columnspan=4, pady=10)
+
+pix = tk.Button(
+    janela,
+    text='Pix',
+    command=abrir_pix, 
+    bg="#DDA0DD",
+    fg='white',
+    font=('Monospace', 12, 'bold')
+)
+pix.grid(row=1, column=1, padx=20, pady=20)
+
+pagamentos = tk.Button(
+    janela,
+    text='Pagamentos',
+    command=abrir_pix, 
+    bg="#DDA0DD",
+    fg='white',
+    font=('Monospace', 12, 'bold')
+)
+pagamentos.grid(row=1, column=2, padx=20, pady=20)
+
+transferir = tk.Button(
+    janela,
+    text='Transferir',
+    command=abrir_pix, 
+    bg="#DDA0DD",
+    fg='white',
+    font=('Monospace', 12, 'bold')
+)
+transferir.grid(row=1, column=3, padx=20, pady=20)
+
+recarga = tk.Button(
+    janela,
+    text='Recarga de celular',
+    command=abrir_pix, 
+    bg="#DDA0DD",
+    fg='white',
+    font=('Monospace', 12, 'bold')
+)
+recarga.grid(row=1, column=4, padx=20, pady=20)
 
 janela.mainloop()
+
 #pix, pagamentos, transferir, recarga de celular 
